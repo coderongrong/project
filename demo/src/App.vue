8e4bda6d72ecec30af8ee726f6c5567773a53836
rong <862405040@qq.com>
2022-11-27 21:33:48 +0800

    1

---
+++ demo/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>123</div>
+  <div>{{ obj.data }}</div>
+</template>
+
+
+<script setup>
+import { reactive } from "vue";
+
+const obj = reactive({
+  data: "abc",
+});
+</script>
