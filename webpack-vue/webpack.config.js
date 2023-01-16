c13d50fe760fe41daeefeace3f405b1be8265edf
rong <862405040@qq.com>
2023-01-16 18:43:30 +0800

    add

---
+++ webpack-vue/webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {resolve} = require('path');
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist')
+    },
+    devServer: {
+        open: true,
+        port: 8080
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, 'src/index.html')
+        })
+    ]
+}
